15fc8b4bdd7222cfeae93dd3c5ae187b2a26d28c
Max <wheels@nyc.rr.com>
2013-12-05 01:44:09 -0500

    initial main page view in angular

--- main.js
+++ main.js
@@ -1,70 +1,82 @@
 angular.module('myApp', ['ui.bootstrap']);
 
-function TodoCtrl($scope) {
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
- 
-  $scope.addTodo = function() {
+function WomCon($scope) {
 
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
- 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
- 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
+  $scope.events = [
+    {imageLocation:'https://lh4.googleusercontent.com/-Yo0xPhkpEkw/AAAAAAAAAAI/AAAAAAAAABg/1_d4z5kPZ8c/w48-c-h48/photo.jpg', categories:{color:'#ff9900'}},
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:{talk:'#ff9900', sport:'#FF0000'}}
+    ];
+
+  $scope.theater = function() {
+    $scope.events[0].categories['color'] = '#FF0000';
+  }
 }
 
+// function TodoCtrl($scope) {
+//   $scope.todos = [
+//     {text:'learn angular', done:true},
+//     {text:'build an angular app', done:false}];
+ 
+//   $scope.addTodo = function() {
 
-function ModalDemoCtrl($scope, $modal, $log) {
+//     $scope.todos.push({text:$scope.todoText, done:false});
+//     $scope.todoText = '';
+//   };
+ 
+//   $scope.remaining = function() {
+//     var count = 0;
+//     angular.forEach($scope.todos, function(todo) {
+//       count += todo.done ? 0 : 1;
+//     });
+//     return count;
+//   };
+ 
+//   $scope.archive = function() {
+//     var oldTodos = $scope.todos;
+//     $scope.todos = [];
+//     angular.forEach(oldTodos, function(todo) {
+//       if (!todo.done) $scope.todos.push(todo);
+//     });
+//   };
+// }
 
-  $scope.items = ['item1', 'item2', 'item3'];
 
-  $scope.open = function () {
+// function ModalDemoCtrl($scope, $modal, $log) {
 
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: ModalInstanceCtrl,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
+//   $scope.items = ['item1', 'item2', 'item3'];
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-};
+//   $scope.open = function () {
 
-function ModalInstanceCtrl($scope, $modalInstance, items) {
+//     var modalInstance = $modal.open({
+//       templateUrl: 'myModalContent.html',
+//       controller: ModalInstanceCtrl,
+//       resolve: {
+//         items: function () {
+//           return $scope.items;
+//         }
+//       }
+//     });
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+//     modalInstance.result.then(function (selectedItem) {
+//       $scope.selected = selectedItem;
+//     }, function () {
+//       $log.info('Modal dismissed at: ' + new Date());
+//     });
+//   };
+// };
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
+// function ModalInstanceCtrl($scope, $modalInstance, items) {
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-};
+//   $scope.items = items;
+//   $scope.selected = {
+//     item: $scope.items[0]
+//   };
+
+//   $scope.ok = function () {
+//     $modalInstance.close($scope.selected.item);
+//   };
+
+//   $scope.cancel = function () {
+//     $modalInstance.dismiss('cancel');
+//   };
+// };
