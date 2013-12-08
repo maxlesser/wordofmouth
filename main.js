88af89b9d4444f2c10e109034b3055bfc6260145
Max <wheels@nyc.rr.com>
2013-12-08 16:41:05 -0500

    filter

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ angular.module('myApp', ['ui.bootstrap']);
 function WomCon($scope, $modal) {
 
   $scope.events = [
-    {imageLocation:'https://lh4.googleusercontent.com/-Yo0xPhkpEkw/AAAAAAAAAAI/AAAAAAAAABg/1_d4z5kPZ8c/w48-c-h48/photo.jpg', categories:{color:'#ff9900'}},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:{talk:'#ff9900', sport:'#FF0000'}, name:"NAME", date:"12/12/13", location:"LOCATION"},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:{talk:'#ff9900', sport:'#FF0000'}},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:{talk:'#ff9900', sport:'#FF0000', social:'#00FFFF', theater:'#00FF00'}},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:{talk:'#ff9900', sport:'#FF0000', social:'#00FFFF'}}
+    {imageLocation:'https://lh4.googleusercontent.com/-Yo0xPhkpEkw/AAAAAAAAAAI/AAAAAAAAABg/1_d4z5kPZ8c/w48-c-h48/photo.jpg', categories:['sport']},
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sport'], name:"NAME", date:"12/12/13", location:"LOCATION"},
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sport']},
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sport', 'social', 'theater']},
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sport', 'social']}
     ];
 
   $scope.users = [
@@ -16,8 +16,10 @@ function WomCon($scope, $modal) {
 
   $scope.currentUser = {};
 
-  $scope.theater = function() {
-    $scope.events[0].categories['color'] = '#FF0000';
+  $scope.categoryFilter = '';
+
+  $scope.filter = function(category) {
+    $scope.categoryFilter = category;
   }
 
   $scope.showOverlay = function(index) {
