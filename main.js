ff50a4825083dccdf6ad61b4599d3f1a47c58f25
Max <wheels@nyc.rr.com>
2013-12-09 03:32:48 -0500

    date/timepicker and view event updates

--- main.js
+++ main.js
@@ -6,10 +6,7 @@ globalCategoryColors = {talk:"#5cb85c", sports:"#5bc0de", social:"#f0ad4e", thea
 function WomCon($scope, $modal) {
 
   $scope.events = [
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sports'], name:"NAME", date:"10-05-2012 11:38:46", location:"LOCATION"},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sports']},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sports', 'social', 'theater']},
-    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', categories:['talk', 'sports', 'social']}
+    {imageLocation:'http://flyingmeat.s3.amazonaws.com/acorn4/images/Acorn256.png', description:'this is a test event! it is being used for test purposes.', categories:['talk', 'sports'], name:"1", date:new Date(), time:'', location:"LOCATION"},
     ];
 
   $scope.users = [
@@ -22,189 +19,191 @@ function WomCon($scope, $modal) {
 
   $scope.categoryFilter = '';
   $scope.searchFilter = '';
+  $scope.date = 'date';
 
   $scope.changeCategoryFilter = function(category) {
     $scope.categoryFilter = category;
   }
 
-  $scope.showOverlay = function(index) {
-  	$scope.events[index].overlay = "true";
+  $scope.showOverlay = function(event) {
+  	event.overlay = "true";
   }
 
-  $scope.hideOverlay = function(index) {
-  	$scope.events[index].overlay = "false";
+  $scope.hideOverlay = function(event) {
+  	event.overlay = "false";
   }
 
-  $scope.openEventModal = function(index) {
-  	  var modalInstance = $modal.open({
-  	    templateUrl: 'eventContent.html',
-  	    controller: EventModalCon,
-  	    resolve: {
-  	      events: function () {
-  	        return $scope.events;
-  	      },
-          index: function () {
-            return index;
-          }
-  	    }
-  	  });
+  $scope.openEventModal = function(event) {
+	  var modalInstance = $modal.open({
+	    templateUrl: 'eventContent.html',
+	    controller: EventModalCon,
+	    resolve: {
+        event: function () {
+          return event;
+        }
+	    }
+	  });
 
-      modalInstance.opened.then(function () {
-        setTimeout(function() {
-          document.getElementById('_name').className = '_summary'; 
-          document.getElementById('_description').className = '_description'; 
-          document.getElementById('_location').className = '_location'; 
-          document.getElementById('_start').className = '_start'; 
-          addthisevent.refresh();
-        }, 1);
-      });
+    modalInstance.opened.then(function () {
+      setTimeout(function() {
+        document.getElementById('_name').className = '_summary'; 
+        document.getElementById('_description').className = '_description'; 
+        document.getElementById('_location').className = '_location'; 
+        document.getElementById('_start').className = '_start'; 
+        addthisevent.refresh();
 
-  	  modalInstance.result.then(function () {
-  	  }, function () {
-  	    // $log.info('Modal dismissed at: ' + new Date());
-  	  });
-  	};
+        document.getElementsByClassName('btn pull-right')[0].className = 'pull-right glyphicon glyphicon-chevron-right'; 
+        document.getElementsByClassName('btn pull-left')[0].className = 'pull-left glyphicon glyphicon-chevron-left';
 
-    $scope.openShoutModal = function(index) {
-      var modalInstance = $modal.open({
-        templateUrl: 'shoutContent.html',
-        controller: ShoutModalCon
-      });
+        document.getElementsByClassName('icon-chevron-up')[0].className = 'glyphicon glyphicon-chevron-up'; 
+        document.getElementsByClassName('icon-chevron-up')[0].className = 'glyphicon glyphicon-chevron-up'; 
+        document.getElementsByClassName('icon-chevron-down')[0].className = 'glyphicon glyphicon-chevron-down';
+        document.getElementsByClassName('icon-chevron-down')[0].className = 'glyphicon glyphicon-chevron-down';
+      }, 1);
+    });
 
-      modalInstance.result.then(function (newEvent) {
-        $scope.events.push(newEvent);
-      }, function () {
-        // $log.info('Modal dismissed at: ' + new Date());
-      });
+	  modalInstance.result.then(function () {
+	  }, function () {
+	    // $log.info('Modal dismissed at: ' + new Date());
+	  });
+	};
+
+  $scope.openShoutModal = function(event) {
+    var modalInstance = $modal.open({
+      templateUrl: 'shoutContent.html',
+      controller: ShoutModalCon
+    });
+
+    modalInstance.result.then(function (newEvent) {
+      $scope.events.push(newEvent);
+    }, function () {
+      // $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.openCreateAccountModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'createAccount.html',
+      controller: CreateAccountCon
+    });
+
+    modalInstance.result.then(function (user) {
+      // this would be validated on the backend if it were a real site, not the frontend
+      // password would also be hashed and then sent, and compared to a hashed pw
+      user.email = user.email.toLowerCase();
+      user.password = user.password.toLowerCase();
+      for (var i = 0; i < $scope.users.length; i++) {
+        if($scope.users[i].email == user.email)
+        {
+          console.log('That email is already in use!!');
+          return;
+        }
+      };
+      
+      console.log('user created!');
+      $scope.users.push(user);
+      $scope.currentUser = user;
+    }, function () {
+      // $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.openLoginModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'login.html',
+      controller: LoginCon
+    });
+
+    modalInstance.result.then(function (user) {
+
+      // this would be validated on the backend if it were a real site, not the frontend
+      // password would also be hashed and then sent, and compared to a hashed pw
+      user.email = user.email.toLowerCase();
+      user.password = user.password.toLowerCase();
+      for (var i = 0; i < $scope.users.length; i++) {
+        if($scope.users[i].email == user.email && $scope.users[i].password == user.password)
+        {
+          console.log('you successfully logged in!');
+          $scope.currentUser = user;
+          return;
+        }
+      };
+
+      console.log('unable to log in - please check email/password combination.');
+    }, function () {
+      // $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  function LoginCon($scope, $modalInstance) {
+    $scope.user = {email:'', password:''};
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.user);
     };
 
-    $scope.openCreateAccountModal = function() {
-      var modalInstance = $modal.open({
-        templateUrl: 'createAccount.html',
-        controller: CreateAccountCon
-      });
+    $scope.close = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }
 
-      modalInstance.result.then(function (user) {
-        // this would be validated on the backend if it were a real site, not the frontend
-        // password would also be hashed and then sent, and compared to a hashed pw
-        user.email = user.email.toLowerCase();
-        user.password = user.password.toLowerCase();
-        for (var i = 0; i < $scope.users.length; i++) {
-          if($scope.users[i].email == user.email)
-          {
-            console.log('That email is already in use!!');
-            return;
-          }
-        };
-        
-        console.log('user created!');
-        $scope.users.push(user);
-        $scope.currentUser = user;
-      }, function () {
-        // $log.info('Modal dismissed at: ' + new Date());
-      });
+  function CreateAccountCon($scope, $modalInstance) {
+
+
+    $scope.user = {email:'', password:''};
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.user);
     };
 
-    $scope.openLoginModal = function() {
-      var modalInstance = $modal.open({
-        templateUrl: 'login.html',
-        controller: LoginCon
-      });
+    $scope.close = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }
 
-      modalInstance.result.then(function (user) {
+  function EventModalCon($scope, $modalInstance, event) {
+    $scope.categoryColors = globalCategoryColors;
 
-        // this would be validated on the backend if it were a real site, not the frontend
-        // password would also be hashed and then sent, and compared to a hashed pw
-        user.email = user.email.toLowerCase();
-        user.password = user.password.toLowerCase();
-        for (var i = 0; i < $scope.users.length; i++) {
-          if($scope.users[i].email == user.email && $scope.users[i].password == user.password)
-          {
-            console.log('you successfully logged in!');
-            $scope.currentUser = user;
-            return;
-          }
-        };
+    $scope.event = event;
+    $scope.editMode = false;
+    $scope.editedEvent = {};
 
-        console.log('unable to log in - please check email/password combination.');
-      }, function () {
-        // $log.info('Modal dismissed at: ' + new Date());
-      });
+    $scope.enterEditMode = function () {
+      $scope.editedEvent = angular.copy($scope.event);
+      $scope.editMode = true;
     };
 
-    function LoginCon($scope, $modalInstance) {
-      $scope.user = {email:'', password:''};
-
-      $scope.ok = function () {
-        $modalInstance.close($scope.user);
-      };
-
-      $scope.close = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    }
-
-    function CreateAccountCon($scope, $modalInstance) {
-
-
-      $scope.user = {email:'', password:''};
-
-      $scope.ok = function () {
-        $modalInstance.close($scope.user);
-      };
-
-      $scope.close = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    }
-
-    function EventModalCon($scope, $modalInstance, events, index) {
-      $scope.categoryColors = globalCategoryColors;
-
-      $scope.event = events[index];
+    $scope.submitEdits = function() {
+      angular.copy($scope.editedEvent, $scope.event);
+      $scope.event.date.setHours($scope.event.time.getHours());
+      $scope.event.date.setMinutes($scope.event.time.getMinutes());
       $scope.editMode = false;
-      $scope.editedEvent = {};
+    };
 
-      $scope.enterEditMode = function () {
-        // $scope.editedEvent = {imageLocation:$scope.event.imageLocation, categories:$scope.event.categories};
-        $scope.editedEvent = angular.copy($scope.event);
-        $scope.editMode = true;
-      };
+    $scope.cancelEdits = function() {
+      $scope.editMode = false;
+    };
 
-      $scope.submitEdits = function() {
-        $scope.event = $scope.editedEvent;
-        events[index] = $scope.event;
-        // console.log(events);
-        // events.splice(index, 1);
-        // events.splice(index, 0, $scope.editedEvents);
-        // console.log(events);
-        $scope.editMode = false;
-      };
+    $scope.close = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }
 
-      $scope.cancelEdits = function() {
-        $scope.editMode = false;
-      };
-
-      $scope.close = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    }
-
-    function ShoutModalCon($scope, $modalInstance) {
+  function ShoutModalCon($scope, $modalInstance) {
 
       $scope.newEvent = {categories:{talk:'#ff9900', sport:'#FF0000'}};
       $scope.image = null;
       $scope.imageFileName = "";
 
-      $scope.ok = function () {
-        $modalInstance.close($scope.newEvent);
-      };
+    $scope.ok = function () {
+      $modalInstance.close($scope.newEvent);
+    };
 
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    }
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
   }
+}
 
 // function TodoCtrl($scope) {
 //   $scope.todos = [
